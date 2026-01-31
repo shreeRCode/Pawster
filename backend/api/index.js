df3067f297d5326f9eed69664f36be8bd467279f
shreeRCode <mshreeraksha3@gmail.com>
2026-01-31 10:33:07 +0530

    CORS

--- backend/api/index.js
+++ backend/api/index.js
@@ -10,14 +10,18 @@ const app = express();
 
 connectDB();
 
+app.use(express.json());
+
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://pawster-2rfz.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   }),
 );
 
-app.use(express.json());
+app.options("*", cors());
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
