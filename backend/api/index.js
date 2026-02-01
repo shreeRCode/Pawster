2e06e5d9b718f1d0d4c638a7edc54aeb80daebe4
shreeRCode <mshreeraksha3@gmail.com>
2026-02-01 19:31:19 +0530

    Added frontend domain

--- backend/api/index.js
+++ backend/api/index.js
@@ -14,7 +14,11 @@ app.use(express.json());
 
 // CORS configuration
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://pawster-2rfz.vercel.app"],
+  origin: [
+    "http://localhost:5173",
+    "https://pawster-2rfz.vercel.app",
+    "https://pawster-2rfz-3i3xn9086-shreerakshas-projects-ae73dc39.vercel.app",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
