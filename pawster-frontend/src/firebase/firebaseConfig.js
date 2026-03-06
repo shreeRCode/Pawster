09f978a42b1c70e7e2670f68098bacf87bed135a
shreeRCode <mshreeraksha3@gmail.com>
2026-03-06 20:23:03 +0530

    use env variables for firebase config

--- pawster-frontend/src/firebase/firebaseConfig.js
+++ pawster-frontend/src/firebase/firebaseConfig.js
@@ -2,20 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your config
 const firebaseConfig = {
-  apiKey: "AIzaSyDRPUiH8gjikv2XtVSC5v324eNDIEgd-iU",
-  authDomain: "pawster-febb6.firebaseapp.com",
-  projectId: "pawster-febb6",
-  storageBucket: "pawster-febb6.appspot.com",
-  messagingSenderId: "539595663996",
-  appId: "1:539595663996:web:1336492c9b6378b5ad77f9",
-  measurementId: "G-L6QRKFL95B",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
